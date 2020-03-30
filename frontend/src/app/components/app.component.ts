bce31099f1ca2124dfee55ab6ca026659ef17277
Stefano Fontana <stefano.fontana.2000@gmail.com>
2020-03-30 12:32:42 +0200

    update skeleton

--- frontend/src/app/components/app.component.ts
+++ frontend/src/app/components/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  templateUrl: '../models/app.component.html',
+  styleUrls: ['../../styles/app.component.sass']
 })
 export class AppComponent {
   title = 'gestionale-sagra-frontend';
