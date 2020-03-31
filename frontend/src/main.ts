6cea476f70cd6d292e415973065d5c9aeb839353
Stefano Fontana <stefano.fontana.2000@gmail.com>
2020-03-31 13:12:23 +0200

    removed shitty code

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,13 +3,6 @@ import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
 import {AppModule} from './app/app.module';
 import {environment} from './environments/environment';
-import LoginManager from "./login/LoginManager";
-import UserPermissionComparator from "./login/comparator/user_permissions/UserPermissionComparator";
-import EventExecution from "./login/EventExecution";
-import StringArrayComparatorContain from "./login/comparator/string_array/StringArrayComparatorContain";
-import AndComparator from "./login/comparator/AndComparator";
-import NotComparator from "./login/comparator/NotComparator";
-import StringArrayComparatorAll from "./login/comparator/string_array/StringArrayComparatorAll";
 
 if (environment.production) {
     enableProdMode();
