4e4356e59b1825a09fc89b57d5eaba83bd5e1d27
Stefano Fontana <stefano.fontana.2000@gmail.com>
2020-03-31 15:15:38 +0200

    login routine

--- frontend/src/login/LoginManager.ts
+++ frontend/src/login/LoginManager.ts
@@ -3,12 +3,14 @@ import Comparator from "./comparator/Comparator";
 import EventExecution from "./EventExecution";
 import Authenticator from "../network/http_client/Authenticator";
 import {authentication_header} from "../settings/settings";
+import {login} from "../settings/requests";
+import LoginResult from "./LoginResult";
 
 let singleton_login_manager = null;
 
 export default class LoginManager implements Authenticator{
 
-    private current_user = null;
+    private _current_user:UserData = null;
 
     private readonly loc_username = "username";
     private readonly loc_name = "name";
@@ -23,7 +25,7 @@ export default class LoginManager implements Authenticator{
     }
 
     private load_user(){
-        this.current_user = new UserData(
+        this._current_user = new UserData(
             localStorage.getItem(this.loc_username),
             localStorage.getItem(this.loc_name),
             JSON.parse(localStorage.getItem(this.loc_permissions))
@@ -39,14 +41,19 @@ export default class LoginManager implements Authenticator{
         return localStorage.getItem(this.loc_is_logged) !== null;
     }
 
-    login(username: string, name: string, permissions: string[], token:string){
-        localStorage.setItem(this.loc_username, username);
-        localStorage.setItem(this.loc_name, name);
-        localStorage.setItem(this.loc_permissions, JSON.stringify(permissions));
-        localStorage.setItem(this.loc_token, token);
-        localStorage.setItem(this.loc_is_logged, "true");
-        this.load_user();
-
+    async login(username: string, password: string): Promise<LoginResult>{
+        let login_res = await login.run({"$username": username, "$password": password}, false);
+        if(login_res.response_code === 403) return new LoginResult(false, login_res.data.message);
+        if(login_res.response_code === 200){
+            localStorage.setItem(this.loc_username, username);
+            localStorage.setItem(this.loc_name, login_res.data.name ?? "UNKNOWN");
+            localStorage.setItem(this.loc_permissions, JSON.stringify(login_res.data.permissions));
+            localStorage.setItem(this.loc_token, login_res.data.token);
+            localStorage.setItem(this.loc_is_logged, "true");
+            this.load_user();
+            return new LoginResult(true, "OK");
+        }
+        return new LoginResult(false, "Unknown error");
     }
 
     logout(){
@@ -58,8 +65,13 @@ export default class LoginManager implements Authenticator{
         this.load_user()
     }
 
+
+    get current_user(): UserData {
+        return this._current_user;
+    }
+
     requireAuth(required_permissions: Comparator<UserData>, fun: any): any {
-        return fun.execute(this.isLogged() && required_permissions.eval(this.current_user))(this.isLogged() ? this.current_user : undefined);
+        return fun.execute(this.isLogged() && required_permissions.eval(this._current_user))(this.isLogged() ? this._current_user : undefined);
     }
 
     doAuthentication(headers, body, oth): boolean {
