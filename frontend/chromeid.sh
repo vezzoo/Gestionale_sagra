b2984bae465f640332dfd135dac8e21502492d8e
Stefano Fontana <stefano.fontana.2000@gmail.com>
2020-04-01 14:33:03 +0200

    chrome app build watch

--- frontend/chromeid.sh
+++ frontend/chromeid.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 export CHROME_APP_CMD="google-chrome --profile-directory=Default"
-export CHROME_APP_ID="fcbongldgohbnfekhbfbhgnmoojdhpdl"
+export CHROME_APP_ID="<your id>"
 export IS_SCRIPT=1
 
 echo $CHROME_APP_CMD
