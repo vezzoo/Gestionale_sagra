3b2c5475fda9f40e51a3439bf017cf45674d8106
Stefano Fontana <stefano.fontana.2000@gmail.com>
2020-04-01 10:50:47 +0200

    ported login to be able to use chrome storage api

--- frontend/src/login/LoginManager.ts
+++ frontend/src/login/LoginManager.ts
@@ -5,6 +5,7 @@ import Authenticator from "../network/http_client/Authenticator";
 import {authentication_header} from "../settings/settings";
 import {login} from "../settings/requests";
 import LoginResult from "./LoginResult";
+import {chrome_local_storage_get, chrome_local_storage_set} from "../settings/chrome_apis";
 
 let singleton_login_manager = null;
 
@@ -18,51 +19,60 @@ export default class LoginManager implements Authenticator{
     private readonly loc_is_logged = "is_logged";
     private readonly loc_token = "tok";
 
-    constructor() {
-        if (localStorage.getItem(this.loc_is_logged) == "true") {
-            this.load_user();
+    private is_logged = false;
+    private token:string;
+
+    constructor() {}
+
+    async load_user(): Promise<void>{
+        if(await await chrome_local_storage_get(this.loc_is_logged)) {
+            this.is_logged = true;
+            this.token = await chrome_local_storage_get(this.loc_token);
+            this._current_user = new UserData(
+                await chrome_local_storage_get(this.loc_username),
+                await chrome_local_storage_get(this.loc_name),
+                JSON.parse(localStorage.getItem(this.loc_permissions))
+            )
+        } else {
+            this.is_logged = false;
+            this.token = "<invalid>";
         }
     }
 
-    private load_user(){
-        this._current_user = new UserData(
-            localStorage.getItem(this.loc_username),
-            localStorage.getItem(this.loc_name),
-            JSON.parse(localStorage.getItem(this.loc_permissions))
-        )
-    }
-
-    static getEnvLogin(): LoginManager {
-        if (singleton_login_manager == null) singleton_login_manager = new LoginManager();
+    static async getEnvLogin(): Promise<LoginManager> {
+        if (singleton_login_manager == null){
+            singleton_login_manager = new LoginManager();
+            await singleton_login_manager.load_user()
+        }
         return singleton_login_manager;
     }
 
-    isLogged(): boolean {
-        return localStorage.getItem(this.loc_is_logged) !== null;
+    async isLogged(): Promise<boolean> {
+        return await chrome_local_storage_get(this.loc_is_logged) !== null;
     }
 
     async login(username: string, password: string): Promise<LoginResult>{
         let login_res = await login.run({"$username": username, "$password": password}, false);
         if(login_res.response_code === 403) return new LoginResult(false, login_res.data.message);
         if(login_res.response_code === 200){
-            localStorage.setItem(this.loc_username, username);
-            localStorage.setItem(this.loc_name, login_res.data.name ?? "UNKNOWN");
-            localStorage.setItem(this.loc_permissions, JSON.stringify(login_res.data.permissions));
-            localStorage.setItem(this.loc_token, login_res.data.token);
-            localStorage.setItem(this.loc_is_logged, "true");
-            this.load_user();
+            await chrome_local_storage_set(this.loc_username, username);
+            await chrome_local_storage_set(this.loc_name, login_res.data.name ?? "UNKNOWN");
+            await chrome_local_storage_set(this.loc_permissions, JSON.stringify(login_res.data.permissions));
+            await chrome_local_storage_set(this.loc_token, login_res.data.token);
+            await chrome_local_storage_set(this.loc_is_logged, "true");
+            await this.load_user();
             return new LoginResult(true, "OK");
         }
         return new LoginResult(false, "Unknown error");
     }
 
-    logout(){
-        localStorage.setItem(this.loc_username, "");
-        localStorage.setItem(this.loc_name, name);
-        localStorage.setItem(this.loc_permissions, JSON.stringify([]));
-        localStorage.setItem(this.loc_token, "");
-        localStorage.setItem(this.loc_is_logged, "notrue");
-        this.load_user()
+    async logout(){
+        await chrome_local_storage_set(this.loc_username, "");
+        await chrome_local_storage_set(this.loc_name, name);
+        await chrome_local_storage_set(this.loc_permissions, JSON.stringify([]));
+        await chrome_local_storage_set(this.loc_token, "");
+        await chrome_local_storage_set(this.loc_is_logged, "notrue");
+        await this.load_user()
     }
 
 
@@ -71,12 +81,12 @@ export default class LoginManager implements Authenticator{
     }
 
     requireAuth(required_permissions: Comparator<UserData>, fun: any): any {
-        return fun.execute(this.isLogged() && required_permissions.eval(this._current_user))(this.isLogged() ? this._current_user : undefined);
+        return fun.execute(this.is_logged && required_permissions.eval(this._current_user))(this.is_logged ? this._current_user : undefined);
     }
 
     doAuthentication(headers, body, oth): boolean {
-        if(!this.isLogged()) return false;
-        headers[authentication_header] = localStorage.getItem(this.loc_token);
+        if(!this.is_logged) return false;
+        headers[authentication_header] = this.token;
         return true;
     }
 }
