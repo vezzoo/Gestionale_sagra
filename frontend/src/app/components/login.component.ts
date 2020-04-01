258a9482fa9182e9c615ff3ee1ad4ba099fc5e20
Luca <luca@implast.it>
2020-04-01 09:26:10 +0200

    Schermata di login finita

--- frontend/src/app/components/login.component.ts
+++ frontend/src/app/components/login.component.ts
@@ -1,19 +1,36 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder} from '@angular/forms';
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
     selector: 'app-login',
     templateUrl: '../models/login.component.html',
-    styleUrls: ['../../styles/login.component.sass']
+    styleUrls: ['../../styles/login.component.sass'],
+    animations: [
+        trigger('errorLoginText', [
+            state('mostra', style({
+                opacity: 1
+            })),
+            state('nascondi', style({
+                opacity: 0
+            })),
+            transition('mostra => nascondi', animate('800ms ease-out')),
+            transition('nascondi => mostra', animate('1000ms ease-in'))
+        ])
+    ]
 })
 
 export class LoginComponent implements OnInit {
     checkoutForm;
-    hide=true;
+
+    hidePassword: boolean = true;
+    loginError: boolean = false;
+
+    errorMessage: String = "Username o password errati";
 
     constructor(private formBuilder: FormBuilder,) {
         this.checkoutForm = this.formBuilder.group({
-            user: '',
+            username: '',
             password: ''
         });
     }
@@ -24,6 +41,23 @@ export class LoginComponent implements OnInit {
 
     onSubmit(data) {
         console.log(data);
+        this.loginError = true;
     }
 
+    getErrorMessage() {
+        this.checkoutForm.patchValue({
+            password: ''
+        });
+
+        return this.errorMessage;
+    }
+
+    onKey(event: any) {
+        if (this.loginError)
+            this.loginError = false;
+    }
+
+    get errorLogin() {
+        return this.loginError ? 'mostra' : 'nascondi';
+    }
 }
