5a6f7fc9f9bddebf5e06883b59787bc00c658edb
Luca <luca@implast.it>
2020-04-01 15:58:23 +0200

    Should work now (1)

--- frontend/chromeid.sh
+++ frontend/chromeid.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 export CHROME_APP_CMD="google-chrome --profile-directory=Default"
-export CHROME_APP_ID="<your id>"
+export CHROME_APP_ID="hoidfnnjpbnmpfhgpbclapijbjjdhfbc"
 export IS_SCRIPT=1
 
 echo $CHROME_APP_CMD
