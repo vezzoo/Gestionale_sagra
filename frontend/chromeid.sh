52dd1ec34a890915228e28f1eacb429a6a694fa0
Stefano Fontana <stefano.fontana.2000@gmail.com>
2020-04-02 11:12:02 +0200

    fixed navigation problems

--- frontend/chromeid.sh
+++ frontend/chromeid.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env sh
 
 export CHROME_APP_CMD="google-chrome --profile-directory=Default"
-export CHROME_APP_ID="<your id>"
+if [ "`(whoami)`" = "stefano" ]; then
+  #tetofonta's app id
+  export CHROME_APP_ID="fcbongldgohbnfekhbfbhgnmoojdhpdl"
+else
+  #vezoo's app id
+  export CHROME_APP_ID="hoidfnnjpbnmpfhgpbclapijbjjdhfbc"
+fi
 export IS_SCRIPT=1
 
 echo $CHROME_APP_CMD
--- frontend/src/app/components/app.component.ts
+++ frontend/src/app/components/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gestionale-sagra-frontend';
+
+  ngOnInit(){
+    history.pushState = function (a) {};
+    history.replaceState = function (a) {};
+  }
 }
