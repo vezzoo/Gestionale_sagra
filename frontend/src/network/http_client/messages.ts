a4154e5e356e2d96c2ab016d8e2e6650fd96a23f
Stefano Fontana <stefano.fontana.2000@gmail.com>
2020-04-15 15:07:32 +0200

    standardized return messages

---
+++ frontend/src/network/http_client/messages.ts
@@ -0,0 +1,9 @@
+import it from "./lang/IT_it"
+
+const dict = it;
+
+export default function (obj: any) {
+    if(obj.code && dict[obj.code]) return dict[obj.code];
+    console.warn("Could not find a translation for", obj.code);
+    return obj.message ?? "UNTRANSLATED";
+}
